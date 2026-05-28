e9f82533892826b2a1e863b92c735f607e0de75e
Newcodes <cabrelmengue555@gmail.com>
2026-05-28 09:08:01 +0100

    Actualite edit

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // Switch between local and production
-  static const bool isProduction = false; // ← change this
+  static const bool isProduction = true; // ← change this
 
   static const String baseUrl = isProduction
       ? 'https://dieumags.pythonanywhere.com/api'
